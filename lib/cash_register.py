5c14b7a919ed0c215467b5cae3ef2504d83cc59a
Sanaya James <sanaya.j.james@gmail.com>
2026-02-09 22:34:59 -0700

    Set up venv. Begun implementing class. Implemented property for discount

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, discount = 0, total = 0):
+    self.discount = discount
+    self.total = total
+    self.items = []
+    self.previous_transactions = []
+
+  @property
+  def discount(self):
+    return self._discount
+  
+  @discount.setter
+  def discount(self, value):
+    if type(value) is int and 0 <= value <= 100:
+      self._discount = value
+    else:
+      print("Not valid discount")
